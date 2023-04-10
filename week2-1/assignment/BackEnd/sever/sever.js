7136c780f3db2f0eaa1e59f2004e59e8e509cc08
Choi-GeonUng <ddugel3@gmail.com>
2023-04-10 18:07:02 +0900

    Sever 구성
    get/patch 구현

---
+++ week2-1/assignment/BackEnd/sever/sever.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+const port = 8000 
+var cors = require('cors')
+
+app.use(cors())
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
+const table = [
+  { team: "String Type Team Name", value: "Number Type Dust Value" },
+  { team: "모각코 2조", value: "38" }
+];
+
+
+app.get("/dust", (req,res) => {
+  res.status(200).json(table)
+})
+
+const Toggle_State = ["ON","OFF"];
+let i=0;
+
+app.patch("/toggle", (req, res) => {
+  res.status(200).json(Toggle_State[i%2])
+  i++;
+})
